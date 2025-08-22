852e709a5826ad1d44bb366ebccb34737459ba81
m.otto <60357676+MichaelOtto1966@users.noreply.github.com>
2025-08-23 00:56:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# Das Verzeichnis, das von CapRover persistent gemacht wird
-PERSISTENT_DIR="/home/coder/$CAPTAIN_APP_NAME"
-TEMP_SOURCE_DIR="/tmp/app_source"
+# Das Verzeichnis, das von CapRover persistent gemacht wird.
+# CapRover garantiert, dass dieses Verzeichnis für jede App eindeutig ist.
+PERSISTENT_DIR="/home/coder/persistent-data"
+TEMP_SOURCE_DIR="/app"
 
 echo "Erstelle persistentes Verzeichnis für $CAPTAIN_APP_NAME..."
 mkdir -p "$PERSISTENT_DIR"
 
 echo "Kopiere Anwendungsdateien in das persistente Verzeichnis..."
-cp -r "$TEMP_SOURCE_DIR/." "$PERSISTENT_DIR"
+# Die -n Option verhindert, dass das Kopieren fehlschlägt, falls das Verzeichnis bereits existiert.
+cp -rn "$TEMP_SOURCE_DIR/." "$PERSISTENT_DIR"
 
 echo "Starte Code-Server..."
 code-server \
